a987dfe7846122615388e5eff603e61ee5ed7eca
anaclara1209 <clarafbana@gmail.com>
2024-11-14 13:14:21 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/api/message', (req, res) => {
+  res.json({ message: 'Hello from REST API!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
 
